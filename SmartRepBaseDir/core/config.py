9495b05a8449758c7b927ea97a96363b85bd1d97
poqoyh <poqoyhartyom@yandex.ru>
2026-05-05 16:51:05 +0300

    Add new settings for working with .env

--- SmartRepBaseDir/core/config.py
+++ SmartRepBaseDir/core/config.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from pydantic import PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
@@ -12,12 +12,18 @@ class ApiPrefix(BaseModel):
 
 class DataBaseConfig(BaseModel):
     url: PostgresDsn
-    echo: bool = False,
-    echo_pool: bool = False,
-    pool_size: int = 50,
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
     max_overflow: int = 10
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template",".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__"
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DataBaseConfig

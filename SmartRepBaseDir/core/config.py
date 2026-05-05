d03c0e2de5002a258a71f6c5fb8701d40a563ebe
poqoyh <poqoyhartyom@yandex.ru>
2026-05-05 15:14:31 +0300

    create database config.py

--- SmartRepBaseDir/core/config.py
+++ SmartRepBaseDir/core/config.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 
 class RunConfig(BaseModel):
@@ -8,8 +9,17 @@ class RunConfig(BaseModel):
 class ApiPrefix(BaseModel):
     api_prefix: str = "/api"
 
+
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False,
+    echo_pool: bool = False,
+    pool_size: int = 50,
+    max_overflow: int = 10
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DataBaseConfig
 
 settings = Settings()

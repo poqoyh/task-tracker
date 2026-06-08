79194779ceaa638b190cd308591a27ba142a1be1
poqoyh <poqoyhartyom@yandex.ru>
2026-06-08 13:41:45 +0300

    Add naming_convection

--- SmartRepBaseDir/core/config.py
+++ SmartRepBaseDir/core/config.py
@@ -2,9 +2,11 @@ from pydantic import BaseModel
 from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
+
 
 class ApiPrefix(BaseModel):
     api_prefix: str = "/api"
@@ -17,15 +19,25 @@ class DataBaseConfig(BaseModel):
     pool_size: int = 50
     max_overflow: int = 10
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".env.template",".env"),
+        env_file=(".env.template", ".env"),
         case_sensitive=False,
         env_nested_delimiter="__",
-        env_prefix="APP_CONFIG__"
+        env_prefix="APP_CONFIG__",
     )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DataBaseConfig
+
 
 settings = Settings()

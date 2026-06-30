8c47d7203adf83f5347b339b28f4c332ac87707a
poqoyh <poqoyhartyom@yandex.ru>
2026-06-30 16:03:20 +0300

    Fix name in encode

---
+++ SmartRepBaseDir/auth/utils.py
@@ -0,0 +1,30 @@
+import jwt
+from core.config import settings
+
+
+def encode_jwt(
+    payload: dict,
+    private_key: str = settings.auth.private_key_path.read_text(),
+    algorithm: str = settings.auth.algorithm,
+):
+    encoded = jwt.encode(
+        payload=payload,
+        key=private_key,
+        algorithm=algorithm,
+    )
+
+    return encoded
+
+
+def decode_jwt(
+    token: str | bytes,
+    public_key=settings.auth.public_key_paths.read_text(),
+    algorithm: str = settings.auth.algorithm,
+):
+    decoded = jwt.decode(
+        token,
+        public_key,
+        algorithms=[algorithm],
+    )
+
+    return decoded

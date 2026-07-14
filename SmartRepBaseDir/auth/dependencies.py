f4690dd05e0822698c9f83838ec4b3836aefa4ba
poqoyh <poqoyhartyom@yandex.ru>
2026-07-14 15:49:54 +0300

    Create get_user_by_token func for get user from token and create get_current_user, get_user_for_refresh_token

--- SmartRepBaseDir/auth/dependencies.py
+++ SmartRepBaseDir/auth/dependencies.py
@@ -11,15 +11,13 @@ from db.models import User
 from .jwt import decode_jwt
 
 
-async def get_current_user(
+async def get_user_by_token(
     request: Request,
-    session: Annotated[
-        AsyncSession,
-        Depends(db_helper.session_getter),
-    ],
+    session: AsyncSession,
+    cookie_name: str,
+    expected_type: str,
 ) -> User:
-
-    token = request.cookies.get("access_token")
+    token = request.cookies.get(cookie_name)
 
     if token is None:
         raise HTTPException(
@@ -30,6 +28,12 @@ async def get_current_user(
     try:
         payload = decode_jwt(token)
     except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid or expired token",
+        )
+
+    if payload.get("type") != expected_type:
         raise HTTPException(
             status_code=401,
             detail="Invalid or expired token",
@@ -52,3 +56,33 @@ async def get_current_user(
         )
 
     return user
+
+
+async def get_current_user(
+    request: Request,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+) -> User:
+    return await get_user_by_token(
+        request=request,
+        session=session,
+        cookie_name="access_token",
+        expected_type="access",
+    )
+
+
+async def get_user_for_refresh_token(
+    request: Request,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+) -> User:
+    return await get_user_by_token(
+        request=request,
+        session=session,
+        cookie_name="refresh_token",
+        expected_type="refresh",
+    )

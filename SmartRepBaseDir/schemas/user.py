943a6713b79fad1d7123f403efff1bd04aed6a38
poqoyh <poqoyhartyom@yandex.ru>
2026-07-14 11:32:11 +0300

    Change UserShortRead shema. Will add user_skill, team later with skills and team shemas

---
+++ SmartRepBaseDir/schemas/user.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserLogin(BaseModel):
+    identifier: str
+    password: str
+
+
+class UserShortRead(UserBase):
+    id: int
+    created_at: datetime
+    # user_skills: list[str] | None
+    # team: str | None

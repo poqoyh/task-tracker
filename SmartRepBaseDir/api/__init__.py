e92a9569a58416b5a59b1754aa4c1bd47f2a7880
poqoyh <poqoyhartyom@yandex.ru>
2026-07-14 11:33:58 +0300

    Create routers for register, login, me for user

--- SmartRepBaseDir/api/__init__.py
+++ SmartRepBaseDir/api/__init__.py
@@ -1,3 +1,9 @@
+from core.config import settings
+
 from fastapi import APIRouter
 
+from .users import router as users_router
+
 router = APIRouter()
+
+router.include_router(users_router, prefix=settings.api.users)
---
+++ SmartRepBaseDir/api/users.py
@@ -0,0 +1,86 @@
+from typing import Annotated
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi import (
+    APIRouter,
+    Depends,
+    Response,
+)
+
+from auth.dependencies import get_current_user
+
+from auth.jwt import (
+    create_access_token,
+    create_refresh_token,
+)
+
+from auth.service import (
+    register_user,
+    authenticate_user,
+)
+
+from db import db_helper
+from db.models import User
+
+from schemas.user import (
+    UserCreate,
+    UserLogin,
+    UserShortRead,
+)
+
+router = APIRouter(tags=["Users"])
+
+
+@router.post("/register/")
+async def register(
+    user_create: UserCreate,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+):
+    return await register_user(
+        session=session,
+        creating_user=user_create,
+    )
+
+
+@router.post("/login/")
+async def login(
+    response: Response,
+    user_login: UserLogin,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+):
+    user = await authenticate_user(session, user_login)
+
+    access_token = create_access_token(user)
+    refresh_token = create_refresh_token(user)
+
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        httponly=True,
+        secure=False,  # Set to True if using HTTPS
+        samesite="lax",
+    )
+
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        secure=False,  # Set to True if using HTTPS
+        samesite="lax",
+    )
+
+    return {"message": "Login successful"}
+
+
+@router.get("/me/", response_model=UserShortRead)
+async def me(
+    current_user: User = Depends(get_current_user),
+):
+    return current_user

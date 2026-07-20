acc1de3ab1acf010ac244247b8f95bc4065a3860
poqoyh <poqoyhartyom@yandex.ru>
2026-07-20 11:14:21 +0300

    Create crud for create/get(skill/skills)/delete user skills

---
+++ SmartRepBaseDir/crud_repositories/user_skill.py
@@ -0,0 +1,64 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from db.models import UserSkill
+
+
+async def create_skill_for_user(
+    session: AsyncSession,
+    user_id: int,
+    skill_id: int,
+    experience_months: int,
+) -> UserSkill:
+
+    user_skill = UserSkill(
+        user_id=user_id,
+        skill_id=skill_id,
+        experience_months=experience_months,
+    )
+
+    session.add(user_skill)
+    await session.commit()
+    await session.refresh(user_skill)
+    return user_skill
+
+
+async def get_user_skill(
+    session: AsyncSession,
+    user_id: int,
+    skill_id: int,
+) -> UserSkill | None:
+
+    stmt = select(UserSkill).where(
+        UserSkill.user_id == user_id,
+        UserSkill.skill_id == skill_id,
+    )
+
+    result = await session.execute(stmt)
+
+    return result.scalar_one_or_none()
+
+
+async def get_users_skills(
+    session: AsyncSession,
+    user_id: int,
+) -> list[UserSkill]:
+
+    stmt = (
+        select(UserSkill)
+        .where(UserSkill.user_id == user_id)
+        .options(selectinload(UserSkill.skill))
+    )
+
+    result = await session.scalars(stmt)
+
+    return result.all()
+
+
+async def delete_user_skill(
+    session: AsyncSession,
+    skill: UserSkill,
+):
+    await session.delete(skill)
+    await session.commit()

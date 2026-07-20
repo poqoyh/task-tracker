429c46151b510ae6cb09be5f3a07bac9075cf39c
poqoyh <poqoyhartyom@yandex.ru>
2026-07-20 14:11:18 +0300

    Add prefix to team routers

--- SmartRepBaseDir/api/__init__.py
+++ SmartRepBaseDir/api/__init__.py
@@ -5,9 +5,11 @@ from fastapi import APIRouter
 from .users import router as users_router
 from .skills import router as skills_router
 from .users_skills import router as users_skills_router
+from .teams import router as team_router
 
 router = APIRouter()
 
 router.include_router(users_router, prefix=settings.api.users)
 router.include_router(skills_router, prefix=settings.api.skills)
 router.include_router(users_skills_router, prefix=settings.api.user_skills)
+router.include_router(team_router, prefix=settings.api.team)

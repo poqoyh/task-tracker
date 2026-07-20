25aa2c35345e4585c05546522cfbeccd1588a114
poqoyh <poqoyhartyom@yandex.ru>
2026-07-20 11:14:50 +0300

    Create service for assign and delete user skill

---
+++ SmartRepBaseDir/service/user_skills.py
@@ -0,0 +1,65 @@
+from fastapi import HTTPException
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from crud_repositories.user_skill import (
+    create_skill_for_user,
+    get_user_skill,
+    delete_user_skill,
+)
+from schemas.user_skill import UserSkillCreate
+
+from service.skills import get_skill_by_id_service
+from auth.service import get_user_by_id_service
+
+
+async def assign_skill_to_user_service(
+    session: AsyncSession,
+    user_id: int,
+    data: UserSkillCreate,
+):
+    await get_user_by_id_service(
+        session=session,
+        user_id=user_id,
+    )
+    await get_skill_by_id_service(
+        session=session,
+        skill_id=data.skill_id,
+    )
+
+    if await get_user_skill(
+        session=session,
+        user_id=user_id,
+        skill_id=data.skill_id,
+    ):
+        raise HTTPException(
+            status_code=409,
+            detail="User already has this skill",
+        )
+
+    return await create_skill_for_user(
+        session=session,
+        skill_id=data.skill_id,
+        user_id=user_id,
+        experience_months=data.experience_months,
+    )
+
+
+async def delete_user_skill_service(
+    session: AsyncSession,
+    user_id: int,
+    skill_id: int,
+):
+    skill = await get_user_skill(
+        session=session,
+        user_id=user_id,
+        skill_id=skill_id,
+    )
+
+    if skill is None:
+        raise HTTPException(
+            status_code=404,
+            detail="skill not found.",
+        )
+
+    await delete_user_skill(session=session, skill=skill)

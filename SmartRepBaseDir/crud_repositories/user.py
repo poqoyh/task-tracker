ea086ad4ab5704624074ff551a69cd00b06f8e58
poqoyh <poqoyhartyom@yandex.ru>
2026-07-20 11:13:31 +0300

    Create funk for get user by id

--- SmartRepBaseDir/crud_repositories/user.py
+++ SmartRepBaseDir/crud_repositories/user.py
@@ -24,6 +24,17 @@ async def create_user(
     return user
 
 
+async def get_user_by_id(
+    session: AsyncSession,
+    user_id: int,
+) -> User | None:
+    result = await session.execute(select(User).where(User.id == user_id))
+
+    user = result.scalar_one_or_none()
+
+    return user
+
+
 async def get_user_by_identifier(
     identifier: str,
     session: AsyncSession,

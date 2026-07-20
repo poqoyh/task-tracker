c6ea42964f45a7d7ca4047dda7f5ba41258e92a2
poqoyh <poqoyhartyom@yandex.ru>
2026-07-20 11:17:07 +0300

    Create api for users skills: post add user skills, get user skills for user and admin, delete user

--- SmartRepBaseDir/api/__init__.py
+++ SmartRepBaseDir/api/__init__.py
@@ -4,8 +4,10 @@ from fastapi import APIRouter
 
 from .users import router as users_router
 from .skills import router as skills_router
+from .users_skills import router as users_skills_router
 
 router = APIRouter()
 
 router.include_router(users_router, prefix=settings.api.users)
 router.include_router(skills_router, prefix=settings.api.skills)
+router.include_router(users_skills_router)
---
+++ SmartRepBaseDir/api/users_skills.py
@@ -0,0 +1,65 @@
+from typing import Annotated
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi import APIRouter, Depends
+
+from auth.dependencies import get_current_user
+from db import db_helper
+from db.models import User
+from schemas.user_skill import UserSkillCreate, UserSkillRead
+
+from service.user_skills import assign_skill_to_user_service, delete_user_skill_service
+
+from crud_repositories.user_skill import get_users_skills
+
+router = APIRouter(tags=["UsersSkills"])
+
+
+@router.post("/user/{user_id}/skill/")
+async def add_skill_to_user(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    user_id: int,
+    data: UserSkillCreate,
+):
+    return await assign_skill_to_user_service(
+        session=session, user_id=user_id, data=data
+    )
+
+
+@router.get("/user/{user_id}/skills/", response_model=list[UserSkillRead])
+async def get_user_skills(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    user_id: int,
+):
+    return await get_users_skills(session=session, user_id=user_id)
+
+
+@router.get("/users/me/skills/", response_model=list[UserSkillRead])
+async def get_my_skills(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    return await get_user_skills(session=session, user_id=int(current_user.id))
+
+
+@router.delete("/user/{user_id}/skills/{skill_id}")
+async def delete_user_skill(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    user_id: int,
+    skill_id: int,
+):
+    await delete_user_skill_service(session=session, user_id=user_id, skill_id=skill_id)
+    return {"message": "Skill deleted successfully."}

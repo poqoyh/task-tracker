68e51043852a3c4a5f3ce9eadf0aa883d47e73c9
poqoyh <poqoyhartyom@yandex.ru>
2026-07-21 14:53:26 +0300

    Create service for update task

--- SmartRepBaseDir/service/tasks.py
+++ SmartRepBaseDir/service/tasks.py
@@ -1,8 +1,9 @@
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from crud_repositories.task import get_task_by_id
+from crud_repositories.task import get_task_by_id, update_task
 from db.models import Task
+from schemas.tasks import TaskUpdate
 
 
 async def get_task_by_id_service(
@@ -21,3 +22,27 @@ async def get_task_by_id_service(
         )
 
     return task
+
+
+async def update_task_service(
+    session: AsyncSession,
+    task_id: int,
+    task_update: TaskUpdate,
+):
+    task = await get_task_by_id_service(
+        session=session,
+        task_id=task_id,
+    )
+    if task is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Task not found.",
+        )
+
+    update_data = task_update.model_dump(exclude_unset=True)
+
+    return await update_task(
+        session=session,
+        task=task,
+        task_update=update_data,
+    )

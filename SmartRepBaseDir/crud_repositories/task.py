6b5f595f29d9735871e5be4fe73da10bc8aedc87
poqoyh <poqoyhartyom@yandex.ru>
2026-07-21 12:33:23 +0300

    Create crud for get all tasks and get task by id

--- SmartRepBaseDir/crud_repositories/task.py
+++ SmartRepBaseDir/crud_repositories/task.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.models import Task
@@ -17,3 +18,25 @@ async def create_task(
     await session.refresh(task)
 
     return task
+
+
+async def get_tasks(
+    session: AsyncSession,
+) -> list[Task] | list[None]:
+
+    stmt = select(Task).order_by(Task.created_at)
+
+    result = await session.scalars(stmt)
+
+    return result.all()
+
+
+async def get_task_by_id(
+    session: AsyncSession,
+    task_id: int,
+) -> Task | None:
+
+    stmt = select(Task).where(Task.id == task_id)
+
+    task = await session.execute(stmt)
+    return task.scalar_one_or_none()

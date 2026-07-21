18adac6454cd7650b5245618c88dff08e6f802d3
poqoyh <poqoyhartyom@yandex.ru>
2026-07-21 12:34:17 +0300

    Create routers for get all tasks and get tasks by id

--- SmartRepBaseDir/api/tasks.py
+++ SmartRepBaseDir/api/tasks.py
@@ -7,11 +7,12 @@ from fastapi import (
     Depends,
 )
 
-from crud_repositories.task import create_task
+from crud_repositories.task import create_task, get_tasks
 
 from db import db_helper
 
 from schemas.tasks import TaskRead, TaskCreate
+from service.tasks import get_task_by_id_service
 
 router = APIRouter(tags=["Tasks"])
 
@@ -25,3 +26,24 @@ async def create(
     task: TaskCreate,
 ):
     return await create_task(session=session, creating_task=task)
+
+
+@router.get("/", response_model=list[TaskRead])
+async def get_all_tasks(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+):
+    return await get_tasks(session=session)
+
+
+@router.get("/{task_id}/", response_model=TaskRead)
+async def get_task(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    task_id: int,
+):
+    return await get_task_by_id_service(session=session, task_id=task_id)

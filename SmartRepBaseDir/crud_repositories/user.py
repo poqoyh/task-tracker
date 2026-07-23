9c354848f08420f9b017043a2d96dd8c280fbbda
poqoyh <poqoyhartyom@yandex.ru>
2026-07-23 13:26:15 +0300

    Create crud for update user

--- SmartRepBaseDir/crud_repositories/user.py
+++ SmartRepBaseDir/crud_repositories/user.py
@@ -68,3 +68,17 @@ async def get_user_by_identifier(
     user = result.scalar_one_or_none()
 
     return user
+
+
+async def update_user(
+    session: AsyncSession,
+    user: User,
+    update_data: dict,
+) -> User:
+    for field, value in update_data.items():
+        setattr(user, field, value)
+
+    await session.commit()
+    await session.refresh(user)
+
+    return user

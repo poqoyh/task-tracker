5115a0e1d14bec37b443f90ef79642bd151e2d82
poqoyh <poqoyhartyom@yandex.ru>
2026-08-14 17:07:07 +0300

    Add confitest with settings for tests

---
+++ task-tracker/tests/conftest.py
@@ -0,0 +1,53 @@
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+
+from main import main_app
+from db import db_helper
+from db.base import Base
+
+TEST_DB_URL = "postgresql+asyncpg://user:password@localhost:5433/smartrepbase_tests"
+
+
+@pytest_asyncio.fixture
+async def engine():
+    test_engine = create_async_engine(TEST_DB_URL, poolclass=NullPool)
+    yield test_engine
+    await test_engine.dispose()
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def prepare_db(engine):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest_asyncio.fixture
+async def client(engine):
+
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+    async def override_session():
+        async with async_session() as session:
+            yield session
+
+    main_app.dependency_overrides[db_helper.session_getter] = override_session
+
+    transport = ASGITransport(app=main_app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        yield ac
+
+    main_app.dependency_overrides.clear()
+
+
+@pytest_asyncio.fixture
+async def session(engine):
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+    async with async_session() as s:
+        yield s

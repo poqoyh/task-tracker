14bcebdcea4f8d4179317432423d293e5d26c765
poqoyh <poqoyhartyom@yandex.ru>
2026-08-19 16:10:47 +0300

    Add: setup.sh for fast creating .env, certs with private/public key, up docker, run alembic migrations

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+echo "Installing dependencies"
+poetry install
+
+if [ ! -f .env ]; then
+    echo "Creating .env."
+    cp .env.template .env
+fi
+
+if [ ! -f certs/private.pem ]; then
+    echo "Generating JWT keys"
+    mkdir -p certs
+    openssl genrsa -out certs/private.pem 2048
+    openssl rsa -in certs/private.pem -pubout -out certs/public.pem
+fi
+
+echo "Starting Database in Docker..."
+docker compose up -d pg
+
+echo "Waiting for PostgreSQL to become healthy."
+until [ "$(docker inspect -f '{{.State.Health.Status}}' task-tracker-pg-1)" == "healthy" ]; do
+    echo "Postgres is initializing..."
+    sleep 2
+done
+echo "Postgres is READY!"
+
+echo "Running migrations"
+poetry run alembic upgrade head
+
+echo "READY! Run: poetry run python main.py"

143816be1e089a98f42bdf678c2e4dfd05be6a45
poqoyh <poqoyhartyom@yandex.ru>
2026-08-21 16:58:00 +0300

    refactor(__init__ from utils, case_converter, db_helper): black reformat

--- db/db_helper.py
+++ db/db_helper.py
@@ -1,31 +1,36 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import (create_async_engine, AsyncEngine, async_sessionmaker, AsyncSession,)
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    async_sessionmaker,
+    AsyncSession,
+)
 
 from core.config import settings
 
 
 class DataBaseHelper:
     def __init__(
-            self,
-            url:str,
-            echo: bool = False,
-            echo_pool: bool = False,
-            max_overflow: int = 10,
-            pool_size: int = 5,
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        max_overflow: int = 10,
+        pool_size: int = 5,
     ) -> None:
         self.engine: AsyncEngine = create_async_engine(
             url=url,
-            echo = echo,
-            echo_pool = echo_pool,
-            max_overflow = max_overflow,
-            pool_size = pool_size,
+            echo=echo,
+            echo_pool=echo_pool,
+            max_overflow=max_overflow,
+            pool_size=pool_size,
         )
 
         self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
-            bind = self.engine,
+            bind=self.engine,
             autoflush=False,
-            autocommit = False,
+            autocommit=False,
             expire_on_commit=False,
         )
 
@@ -38,7 +43,7 @@ class DataBaseHelper:
 
 
 db_helper = DataBaseHelper(
-    url = str(settings.db.url),
+    url=str(settings.db.url),
     echo=settings.db.echo,
     echo_pool=settings.db.echo_pool,
     max_overflow=settings.db.max_overflow,

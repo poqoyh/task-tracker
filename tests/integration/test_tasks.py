a8e2831e93247910281b7e614d2c2d96e2390745
poqoyh <poqoyhartyom@yandex.ru>
2026-08-25 15:07:16 +0300

    create(test/integration/test_task): test for Assign task to user with permission

--- tests/integration/test_tasks.py
+++ tests/integration/test_tasks.py
@@ -1,5 +1,7 @@
 import pytest
 
+from db.models import Task
+
 pytest = pytest.mark.asyncio
 
 
@@ -347,3 +349,173 @@ async def test_worker_update_his_task(
     body = response.json()
 
     assert body["detail"] == "Not enough permissions to update this task"
+
+
+"""
+Assign task to user
+"""
+
+
+async def test_test_admin_assign_task_to_user(
+    worker_user,
+    admin_client,
+    create_task,
+):
+    task = await create_task()
+
+    response = await admin_client.post(
+        f"/api/task/{task['id']}/assign/{worker_user.id}"
+    )
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert body["id"] == 1
+    assert body["user_id"] == 1
+
+
+async def test_team_lead_assign_task_to_worker(
+    session,
+    worker_user,
+    team_lead_client,
+    team_lead_user,
+    create_task,
+    create_team,
+):
+    task = await create_task()
+    team = await create_team()
+
+    team_lead_user.team_id = team["id"]
+    worker_user.team_id = team["id"]
+
+    session.add_all([team_lead_user, worker_user])
+    await session.commit()
+
+    response = await team_lead_client.post(
+        f"/api/task/{task['id']}/assign/{worker_user.id}"
+    )
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert body["id"] == 1
+    assert body["user_id"] == 1
+
+
+async def test_team_lead_assign_task_to_worker_in_another_team(
+    session,
+    worker_user,
+    team_lead_client,
+    team_lead_user,
+    create_task,
+    create_team,
+):
+    task = await create_task()
+
+    team = await create_team()
+    team_2 = await create_team(name="Frontend", description="Frontend Team")
+
+    team_lead_user.team_id = team["id"]
+    worker_user.team_id = team_2["id"]
+
+    session.add_all([team_lead_user, worker_user])
+    await session.commit()
+
+    response = await team_lead_client.post(
+        f"/api/task/{task['id']}/assign/{worker_user.id}"
+    )
+
+    assert response.status_code == 403
+
+    body = response.json()
+
+    assert body["detail"] == "Not enough permissions to assign this task"
+
+
+async def test_team_lead_without_team_assign_task_to_worker(
+    session,
+    worker_user,
+    team_lead_client,
+    create_task,
+    create_team,
+):
+    task = await create_task()
+
+    team = await create_team()
+
+    worker_user.team_id = team["id"]
+
+    session.add(worker_user)
+    await session.commit()
+
+    response = await team_lead_client.post(
+        f"/api/task/{task['id']}/assign/{worker_user.id}"
+    )
+
+    assert response.status_code == 403
+
+    body = response.json()
+
+    assert body["detail"] == "Not enough permissions to assign this task"
+
+
+async def test_worker_assign_task(
+    session,
+    worker_client,
+    worker_user,
+    create_task,
+    create_team,
+):
+    task = await create_task()
+
+    team = await create_team()
+
+    worker_user.team_id = team["id"]
+
+    session.add(worker_user)
+    await session.commit()
+
+    response = await worker_client.post(
+        f"/api/task/{task['id']}/assign/{worker_user.id}"
+    )
+
+    assert response.status_code == 403
+
+    body = response.json()
+
+    assert body["detail"] == "Not enough permissions to assign this task"
+
+
+async def test_admin_assign_task_that_has_already_been_assigned(
+    session,
+    admin_client,
+    worker_user,
+    create_task,
+    create_team,
+):
+    task = await create_task()
+
+    team = await create_team()
+
+    worker_user.team_id = team["id"]
+
+    session.add(worker_user)
+    await session.commit()
+
+    first_response = await admin_client.post(
+        f"/api/task/{task['id']}/assign/{worker_user.id}"
+    )
+
+    assert first_response.status_code == 200
+
+    response = await admin_client.post(
+        f"/api/task/{task['id']}/assign/{worker_user.id}"
+    )
+
+    assert response.status_code == 409
+
+    body = response.json()
+
+    assert body["detail"] == "Task already assigned."

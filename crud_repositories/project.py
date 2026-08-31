999436ea10021018993f8a9747466e9eeb56bc4a
poqoyh <poqoyhartyom@yandex.ru>
2026-08-31 13:30:42 +0300

    create(crud_repositories/project): create, get by id, get all, update and count

---
+++ crud_repositories/project.py
@@ -0,0 +1,65 @@
+from sqlalchemy import select, func
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models import Project
+
+
+async def create_project(
+    session: AsyncSession,
+    project_data: dict,
+) -> Project:
+
+    project = Project(**project_data)
+
+    session.add(project)
+
+    await session.commit()
+    await session.refresh(project)
+
+    return project
+
+
+async def get_project_by_id(
+    session: AsyncSession,
+    project_id: int,
+) -> Project | None:
+
+    stmt = select(Project).where(Project.id == project_id)
+
+    result = await session.execute(stmt)
+
+    return result.scalar_one_or_none()
+
+
+async def get_projects(
+    session: AsyncSession,
+    limit: int,
+    offset: int,
+) -> list[Project]:
+
+    stmt = select(Project).order_by(Project.name).limit(limit).offset(offset)
+
+    result = await session.scalars(stmt)
+
+    return result.all()
+
+
+async def count_projects(session: AsyncSession) -> int:
+    result = await session.scalar(select(func.count()).select_from(Project))
+
+    return result or 0
+
+
+async def update_project(
+    session: AsyncSession,
+    project: Project,
+    update_data: dict,
+) -> Project:
+
+    for field, value in update_data.items():
+        setattr(project, field, value)
+
+    await session.commit()
+    await session.refresh(project)
+
+    return project
